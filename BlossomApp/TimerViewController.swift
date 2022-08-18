e06d15a0057cc712eac4b7b67c36051da24f2e1e
Angelina <angelinamariesanchez59@gmail.com>
2022-08-18 07:58:31 -0700

    coded timer

--- BlossomApp/TimerViewController.swift
+++ BlossomApp/TimerViewController.swift
@@ -1,13 +1,68 @@
 //
-//  TimerViewController.swift
+//  TImerViewController.swift
 //  BlossomApp
 //
-//  Created by Janice Sanchez on 8/17/22.
+//  Created by Janice Sanchez on 8/18/22.
 //
 
 import UIKit
 
-class TimerViewController: UIViewController {
+class TImerViewController: UIViewController {
+    
+    var minutes = 25
+    var timer = Timer()
+    
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func slider(_ sender: UISlider) {
+        minutes = Int(sender.value)
+        lblTime.text = String(minutes)
+    }
+    
+    @IBAction func btnStart(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector (TImerViewController.counter), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func btnStop(_ sender: Any) {
+        timer.invalidate()
+        minutes = 25
+        slider.setValue(25, animated: true)
+        lblTime.text = "25:00"
+    }
+    
+    @objc func counter () {
+        minutes -= 1
+        //lblTime.text = String(seconds)
+        lblTime.text = timeString(time: TimeInterval(minutes))
+        
+        if (minutes == 0) {
+            timer.invalidate()
+            Alert()
+        }
+        slider.value = Float(minutes)
+    }
+    
+    func timeString(time:TimeInterval) -> String {
+        let hours = Int(time) / 3600
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    
+    func Alert() {
+        let alertController = UIAlertController(title: "Time's up!", message: "Please take a five minute break :)", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            print("cancel")
+        }
+        alertController.addAction(cancelAction)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            print("OK")
+        }
+        alertController.addAction(OKAction)
+        self.present(alertController, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
