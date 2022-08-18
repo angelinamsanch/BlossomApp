7d47efb7a40b9cc8675ed2e91eab6b60f97f02ae
Angelina <angelinamariesanchez59@gmail.com>
2022-08-18 10:26:37 -0700

    timer

--- BlossomApp/TimerViewController.swift
+++ BlossomApp/TimerViewController.swift
@@ -12,47 +12,68 @@ class TImerViewController: UIViewController {
     var minutes = 25
     var timer = Timer()
     
+    
+    
+    
     @IBOutlet weak var lblTime: UILabel!
     @IBOutlet weak var slider: UISlider!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
     
     @IBAction func slider(_ sender: UISlider) {
         minutes = Int(sender.value)
         lblTime.text = String(minutes)
     }
     
-    @IBAction func btnStart(_ sender: Any) {
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector (TImerViewController.counter), userInfo: nil, repeats: true)
+       
+    
+
+    @IBAction func buttonStart(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TImerViewController.updateTimer), userInfo: nil, repeats: true)
     }
     
-    @IBAction func btnStop(_ sender: Any) {
+    
+    
+    @IBAction func buttonStop(_ sender: Any) {
         timer.invalidate()
         minutes = 25
         slider.setValue(25, animated: true)
-        lblTime.text = "25:00"
+        lblTime.text = String(minutes)
     }
     
-    @objc func counter () {
+    @objc func updateTimer () {
+        
         minutes -= 1
-        //lblTime.text = String(seconds)
         lblTime.text = timeString(time: TimeInterval(minutes))
         
-        if (minutes == 0) {
-            timer.invalidate()
-            Alert()
-        }
         slider.value = Float(minutes)
+        
+        if minutes == 0 {
+            timer.invalidate()
+        }
     }
     
     func timeString(time:TimeInterval) -> String {
+        
         let hours = Int(time) / 3600
-        let minutes = Int(time) / 60 % 60
+        let minutes = Int(time) / 60 
         let seconds = Int(time) % 60
-        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+        
+        return String(format: "%02i:%02i:%02i", hours,minutes,seconds)
+        
     }
     
-    func Alert() {
-        let alertController = UIAlertController(title: "Time's up!", message: "Please take a five minute break :)", preferredStyle: .alert)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+    func alert () {
+        let alertController = UIAlertController(title: "Time's up!", message: "please take a 5 minute break :)", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel) { (action) in
             print("cancel")
         }
         alertController.addAction(cancelAction)
@@ -62,13 +83,12 @@ class TImerViewController: UIViewController {
         }
         alertController.addAction(OKAction)
         self.present(alertController, animated: true)
+        
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
+    
+    
     
 
     /*
